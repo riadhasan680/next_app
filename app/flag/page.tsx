23f20eb2b9c27eca9a7f7ba0ac787dd7011058e9
riadhasan680 <hasanahmed7393@gmail.com>
2025-11-17 21:46:46 +0600

    remove flag item from dashboard sidebar

--- app/flag/page.tsx
+++ app/flag/page.tsx
@@ -2,7 +2,7 @@ export default function FlagStandalonePage() {
   return (
     <main className="min-h-screen bg-white flex items-center justify-center">
       <div className="px-4 sm:px-6 lg:px-8 py-10">
-        <p className="font-mono text-lg ">BNCTF{`WH1TE_ON_WH1TE_IN_TR4FF1K`}</p>
+        <p className="font-mono text-lg text-white">BNCTF{`WH1TE_ON_WH1TE_IN_TR4FF1K`}</p>
       </div>
     </main>
   )
